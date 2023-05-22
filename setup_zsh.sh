7b33086f2395c1d35d356d9f7ce4824d24ef3444
Cezary Klamra <cklamra@icloud.com>
2023-05-22 17:50:49 +0200

    zsh setup added

---
+++ setup_zsh.sh
@@ -0,0 +1,4 @@
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+cp alanpeabody.zsh-theme ~/.oh-my-zsh/themes/
+cp zshrc ~/.zshrc
+chsh -s $(which zsh)
