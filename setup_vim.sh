40998ee0f8e5d8127b6b667566a227b7d4bc3bcb
Cezary Klamra <cklamra@icloud.com>
2023-09-02 12:37:24 +0200

    updates

--- setup_vim.sh
+++ setup_vim.sh
@@ -6,19 +6,22 @@ python3 -m pip install flake8
 
 echo "
 call plug#begin()
-Plug 'vimpostor/vim-lumen' \" autodarkmode mac/linux
+\"Plug 'vimpostor/vim-lumen'
+\" autodarkmode mac/linux
 Plug 'preservim/nerdtree'
 Plug 'tmhedberg/SimpylFold'
 Plug 'vim-airline/vim-airline'
 \"Plug 'vim-airline/vim-airline-themes'
 Plug 'dense-analysis/ale'
-\"Plug 'joshdick/onedark.vim'
 \"Plug 'prabirshrestha/vim-lsp'
 Plug 'Vimjas/vim-python-pep8-indent'
+Plug 'joshdick/onedark.vim'
 \"Plug 'sainnhe/everforest'
 \"Plug 'rakr/vim-one'
-Plug 'sonph/onehalf', { 'rtp': 'vim' }
+\"Plug 'sonph/onehalf', { 'rtp': 'vim' }
+Plug 'chrisbra/csv.vim'
 Plug 'neoclide/coc.nvim', {'branch': 'release'}
+Plug 'haya14busa/incsearch.vim'
 call plug#end()
 
 syntax on
@@ -31,7 +34,7 @@ set smartindent
 set ic
 set mouse=a
 
-if has('persistent_undo') 
+if has('persistent_undo')
   set undodir=$HOME/.vim/undo
   set undofile
 endif
@@ -40,12 +43,12 @@ endif
 \"  set termguicolors
 \"endif
 
-au User LumenLight colorscheme onehalflight 
-au User LumenLight let g:airline_theme='onehalflight'
-au User LumenDark colorscheme onehalfdark
-au User LumenDark let g:airline_theme='onehalfdark'
-\"colorscheme onedark
-\"autocmd vimenter * hi EndOfBuffer guibg=NONE ctermbg=NONE
+\"au User LumenLight colorscheme onehalflight
+\"au User LumenLight let g:airline_theme='onehalflight'
+\"au User LumenDark colorscheme onehalfdark
+\"au User LumenDark let g:airline_theme='onehalfdark'
+colorscheme onedark
+\"autocmd VimEnter * hi EndOfBuffer guibg=NONE ctermbg=NONE
 \"let g:everforest_background='hard' \" 'soft', 'medium', 'hard'
 \"let g:everforest_better_performance=1
 \"colorscheme everforest
@@ -70,7 +73,14 @@ nnoremap <A-F6> 6gt
 nnoremap <A-F7> 7gt
 nnoremap <A-F8> 8gt
 nnoremap <A-F9> 9gt
-nnoremap <A-F10> 10gt 
+nnoremap <A-F10> 10gt
+
+\" airline symbols
+\"let g:airline_powerline_fonts = 1
+let g:airline_symbols = {}
+let g:airline_symbols.maxlinenr = ''
+let g:airline_symbols.colnr = ' '
+let g:airline_symbols.linenr = ' '
 
 \" F5 to execute python
 autocmd FileType python nnoremap <buffer> <F5> :w<cr>:exec '!clear'<cr>:exec '!python3' shellescape(expand('%:p'), 1)<cr>
@@ -80,17 +90,28 @@ set foldmethod=indent
 set foldlevel=99
 nnoremap <space> za
 
+\" search highlighting
+set hlsearch
+let g:incsearch#auto_nohlsearch = 1
+map n  <Plug>(incsearch-nohl-n)
+map N  <Plug>(incsearch-nohl-N)
+map *  <Plug>(incsearch-nohl-*)
+map #  <Plug>(incsearch-nohl-#)
+map g* <Plug>(incsearch-nohl-g*)
+map g# <Plug>(incsearch-nohl-g#)
+
+
 \" autocompletion, linging
 let g:ale_linters = {'python': ['flake8', 'vulture']}
 let g:ale_completion_enabled = 0
 let g:ale_virtualtext_cursor = 'disabled'
 let g:ale_sign_error = '!'
-let g:ale_sign_warning= '>'
+let g:ale_sign_warning = '>'
 let g:ale_sign_info = '~'
 let g:ale_sign_style_error = '–'
 let g:ale_sign_style_warning = '-'
 
-\" Coc config
+\" coc config
 inoremap <silent><expr> <TAB>
       \\ coc#pum#visible() ? coc#pum#next(1) :
       \\ CheckBackspace() ? \"<Tab>\" :
