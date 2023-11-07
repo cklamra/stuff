c61da07fe0c2520f9dd934e8993ac3983f8a2bc4
Cezary Klamra <cklamra@icloud.com>
2023-11-07 20:12:54 +0100

    plymouth theme, gnome icons

---
+++ fix_morewaita.sh
@@ -0,0 +1,5 @@
+cp ~/.local/share/icons/hicolor/scalable/apps/firefox.svg ~/.local/share/icons/MoreWaita/apps/scalable/
+cp ~/.local/share/icons/hicolor/scalable/apps/org.gnome.Shell.Extensions.GSConnect-symbolic.svg ~/.local/share/icons/MoreWaita/apps/scalable
+cp ~/.local/share/icons/hicolor/scalable/apps/org.gnome.Shell.Extensions.GSConnect-symbolic.svg ~/.local/share/icons/MoreWaita/apps/scalable
+cp ~/.local/share/icons/hicolor/symbolic/apps/com.raggesilver.BlackBox-symbolic.svg ~/.local/share/icons/MoreWaita/apps/symbolic
+cp ~/.local/share/icons/hicolor/symbolic/apps/firefox-symbolic.svg ~/.local/share/icons/MoreWaita/apps/symbolic
