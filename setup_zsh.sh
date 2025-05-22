fb7bc51331c4634ff7931d9a813f3c08ad16a1a5
Cezary Klamra <cklamra@icloud.com>
2025-05-22 16:45:53 +0200

    ghostty theme

--- setup_zsh.sh
+++ setup_zsh.sh
@@ -2,4 +2,3 @@ sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/too
 cp alanpeabody.zsh-theme ~/.oh-my-zsh/themes/
 cp zshrc ~/.zshrc
 chsh -s $(which zsh)
-./pyenv_config.sh
